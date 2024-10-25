7d97dc777d430afc080e62e4578a80574e053b8b
uno092102 <claivejustinbarrientos12@gmail.com>
2024-10-25 14:44:27 +0800

    first commit

---
+++ src/main/java/com/finalproject/campusnavigator/CampusnavigatorApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.campusnavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusnavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusnavigatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/campusnavigator/CampusnavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.campusnavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusnavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
