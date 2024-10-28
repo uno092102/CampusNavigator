3ae303728c3648d058fb160f50ee6626aa7a1925
brentamage <brentgalpez@gmail.com>
2024-10-28 10:56:57 +0800

    User api Partial done

--- src/main/java/com/campusnavigator/Controller/UserController.java
+++ src/main/java/com/campusnavigator/Controller/UserController.java
@@ -1,13 +1,22 @@
 package com.campusnavigator.Controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.campusnavigator.Entity.User;
 import com.campusnavigator.Service.UserService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @RestController
@@ -16,10 +25,37 @@ public class UserController {
 
     @Autowired
     UserService userv;
-    @GetMapping("path")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    //test
+    @GetMapping("/print")
+    public String print()
+    {
+        return "TEST TEST";
     }
     
-    
+    //create
+    @PostMapping("/postSearchEntity")
+    public User postUserRecord(@RequestBody User search)
+    {
+        return userv.postUserRecord(search);
+    }
+    //read
+    @GetMapping("/getAllSearch")
+    public List<User>getAllUser() {
+        return userv.getAllUser();
+    }
+    //update
+    @PutMapping("/putUserRecord")
+    public User putUserHistory(@RequestParam int userID, @RequestBody User newUser)
+    {
+        return userv.putUser(userID, newUser);
+    }    
+    //delete
+    @DeleteMapping("/deleteUser/{userID}")
+    public String deleteUser(@PathVariable int userID)
+    {
+        return userv.deleteUser(userID);
+    }
+        
 }
+    
+
