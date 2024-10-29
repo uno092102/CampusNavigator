a7779d952a47f7c25d3c0c4cd8e21ff187094359
MasuRii Math Lee マス リ <kanjiharigana@gmail.com>
2024-10-29 23:33:36 +0800

    Remove comments

--- frontend/campusnavigator/src/pages/login.jsx
+++ frontend/campusnavigator/src/pages/login.jsx
@@ -1,4 +1,3 @@
-// src/pages/login.jsx
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './signup.css';
--- frontend/campusnavigator/src/pages/signup.jsx
+++ frontend/campusnavigator/src/pages/signup.jsx
@@ -1,4 +1,3 @@
-// src/pages/signup.jsx
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './signup.css';
