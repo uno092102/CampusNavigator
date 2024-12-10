5073471d52a3dcd325ebbc034f99291fe165f250
MasuRii マス リ <kanjiharigana@gmail.com>
2024-12-10 12:46:38 +0800

    Add: included is_admin boolean attribute

--- src/main/java/com/campusnavigator/Entity/User.java
+++ src/main/java/com/campusnavigator/Entity/User.java
@@ -2,40 +2,42 @@ package com.campusnavigator.Entity;
 
 import jakarta.persistence.*;
 import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class User {
 
-    @Id
+    @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userID;
-
+    
     private String name;
     private String role;
     private String email;
     private String password;
+    private boolean admin; // Added attribute to represent admin status
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true) // Relationship mapping
-    @JsonManagedReference
-    private List<GeolocationData> geolocationData; // A user can have multiple geolocation entries
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference 
+    private List<GeolocationData> geolocationData;
 
     // Default constructor
     public User() {
         super();
     }
 
-    // Parameterized constructor
-    public User(int userID, String name, String role, String email, String password) {
+    // Parameterized constructor including the new 'admin' attribute
+    public User(int userID, String name, String role, String email, String password, boolean admin) {
         this.userID = userID;
         this.name = name;
         this.role = role;
         this.email = email;
         this.password = password;
+        this.admin = admin;
     }
 
-    // Getters and Setters for basic fields
+    // Getters and Setters for all attributes, including 'admin'
+
     public int getUserID() {
         return userID;
     }
@@ -52,36 +54,42 @@ public class User {
         this.name = name;
     }
 
-    public String getRole() {
+   public String getRole() {
         return role;
     }
 
     public void setRole(String role) {
         this.role = role;
     }
-
+ 
     public String getEmail() {
         return email;
     }
-
+  
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPassword() {
+   public String getPassword() {
         return password;
     }
-
+ 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    // Getter for geolocationData
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
     public List<GeolocationData> getGeolocationData() {
         return geolocationData;
     }
 
-    // Setter for geolocationData
     public void setGeolocationData(List<GeolocationData> geolocationData) {
         this.geolocationData = geolocationData;
     }
